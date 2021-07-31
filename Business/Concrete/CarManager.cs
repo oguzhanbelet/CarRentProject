c489f6aac55c8b52359f331e1c04079f452b1137
Oğuzhan Belet <oguzhanbelet@gmail.com>
2021-07-31 18:46:27 +0300

    12. Ders Ödevlerim

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -21,12 +23,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.DailyPrice<100)
-            {
-                return new ErrorResult(Messages.CarNameInvalid);
-            }
             _carDal.Add(car);
 
             return new SuccessResult(Messages.CarAdded);
@@ -47,11 +46,6 @@ namespace Business.Concrete
 
         public IDataResult<List<Car>> GetAll()
         {
-            if (DateTime.Now.Hour==22)
-            {
-                return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
-            }
-
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.CarsListed);
         }
 
--- Business/ValidationRules/FluentValidation/CarValidator.cs
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -8,13 +8,13 @@ using System.Threading.Tasks;
 
 namespace Business.ValidationRules.FluentValidation
 {
-    public class CarValidator:AbstractValidator<Car>
+    public class CarValidator : AbstractValidator<Car>
     {
         public CarValidator()
         {
             RuleFor(c => c.DailyPrice).NotEmpty();
             RuleFor(c => c.ModelYear).NotEmpty();
-            RuleFor(c => c.ModelYear).GreaterThan(1900);
+            RuleFor(c => c.ModelYear).GreaterThanOrEqualTo(1900);
             RuleFor(c => c.Description).NotEmpty();
             RuleFor(c => c.Description).MinimumLength(3);
             RuleFor(c => c.DailyPrice).GreaterThan(0);
